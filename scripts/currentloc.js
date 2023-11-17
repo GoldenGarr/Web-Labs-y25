39d4af4ec78c182ad467121b478b5c62067dbfa5
GoldenGarr <GoldenGarr@>
2023-11-17 22:46:42 +0300

    remove: cart

--- scripts/currentloc.js
+++ scripts/currentloc.js
@@ -2,14 +2,6 @@ document.addEventListener('DOMContentLoaded', function () {
     let menuItems = document.querySelectorAll('.menu-item');
 
     menuItems.forEach(function (item) {
-        item.addEventListener('click', function (event) {
-            menuItems.forEach(function (item) {
-                item.classList.remove('active');
-            });
-
-            event.target.classList.add('active');
-        });
-
         if (window.location.href.includes(item.getAttribute('href'))) {
             item.classList.add('active');
         }
--- scripts/loadtime.js
+++ scripts/loadtime.js
@@ -2,7 +2,6 @@
         window.addEventListener('load', function () {
             let loadTime = window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart;
             let footer = document.querySelector('footer');
-            // footer.innerHTML = 'Page load time: ' + loadTime + 'ms';
 
             let loadTimeElement = document.createElement('div')
             let text = document.createTextNode('Page load time: ' + loadTime + 'ms')
@@ -12,29 +11,3 @@
     }
 )
 ();
-
-
-// alert(document.location)
-
-// window.document.write("")
-
-// str = document.location;
-// alert(str)
-
-// function GFG_FUN() {
-//     str = str.split("/");
-//
-//     alert(str[str.length - 1]);
-// }
-//
-// GFG_FUN();
-
-// alert(str.split("/"))
-
-
-// window.document.write(str[str.length - 1]);
-
-
-// window.document.write(document.location)
-// window.document.write(document.location.split('/')[-1])
-
